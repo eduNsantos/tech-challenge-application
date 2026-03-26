6b7b9cf80a99be3ff3879cd163e044086d4589ff
Eduardo Santos <edu.nascimento22@outlook.com>
2026-03-25 23:50:32 -0300

    feat: list vehicles

--- app/Domain/Vehicle/Repositories/VehicleRepositoryInterface.php
+++ app/Domain/Vehicle/Repositories/VehicleRepositoryInterface.php
@@ -3,9 +3,14 @@
 namespace App\Domain\Vehicle\Repositories;
 
 use App\Domain\Vehicle\Entities\Vehicle;
+use App\Infrastructure\Persistence\Eloquent\Models\VehicleModel;
 
 interface VehicleRepositoryInterface
 {
     public function save(Vehicle $vehicle): void;
+    /**
+     * @return VehicleModel[]
+     */
+    public function findAll(): array;
     public function findByPlate(string $plate): ?Vehicle;
 }
