70eff6765e5bd47ea5133ebe89881d6d62c2f120
Eduardo Santos <edu.nascimento22@outlook.com>
2026-03-26 13:50:04 -0300

    fix: route fix

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Interfaces\Http\Controllers\VehicleController;
+use App\Presentation\Http\Controllers\VehicleController;
 
 Route::group([
     'middleware' => 'api',
