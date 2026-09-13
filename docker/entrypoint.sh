e054ade566680d9b8277deedb21fcbc0b2afe707
Eduardo Santos <edu.nascimento22@outlook.com>
2026-09-13 20:37:53 -0300

    fix entry point

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-if [ -n "${NEW_RELIC_API_KEY:-}" ] && [ -n "${NEW_RELIC_ACCOUNT_ID:-}" ]; then
-    /usr/local/bin/newrelic install -n logs-integration
+if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
+    php artisan migrate --force --no-interaction || exit $?
 fi
 
 exec "$@"
