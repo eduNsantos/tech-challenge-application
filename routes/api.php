fe4e0ab0f271f9515c32b887511108618bb6b417
Eduardo Santos <edu.nascimento22@outlook.com>
2026-09-15 01:49:44 -0300

    teste error failre route

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,20 @@ Route::get('/up', function () {
     ]);
 });
 
+Route::get('/debug/service-order/failure', function () {
+    if (function_exists('newrelic_record_metric')) {
+        newrelic_record_metric('Custom/ServiceOrder/Failure', 1);
+        newrelic_add_custom_parameter('service_order_test', 'manual_trigger');
+        newrelic_notice_error('Manual trigger: service order processing failure test');
+    }
+
+    return response()->json([
+        'status' => 'ok',
+        'metric' => 'Custom/ServiceOrder/Failure',
+        'trigger' => 'manual_trigger',
+    ]);
+});
+
 Route::get('/service-order/approve/{token}', [ServiceOrderApprovalController::class, 'approve']);
 Route::get('/service-order/reject/{token}', [ServiceOrderApprovalController::class, 'reject']);
 Route::post('/service-order/approval/{token}', [ServiceOrderApprovalController::class, 'handle']);
